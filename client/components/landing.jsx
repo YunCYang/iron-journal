1cd0afc2aafc2469e15228e65e8a458ac314c435
YunCYang <ericeorl2013@gmail.com>
2020-06-10 22:28:44 -0700

    working on input

--- client/components/landing.jsx
+++ client/components/landing.jsx
@@ -19,10 +19,26 @@ const Landing = () => {
           <span>Iron Journal</span>
         </div>
         <div className="landing-title__secondary">
-          <span>An journal for RPG Ironsworn</span>
+          <span>A journal for the RPG Ironsworn</span>
         </div>
       </div>
-      <div className="landing-action"></div>
+      <div className="landing-action">
+        <div className="landing-action__userName">
+          <input type="text" name="userName" id="userName"/>
+          <span className="landing-action__highlight"></span>
+          <span className="landing-action__bar"></span>
+          <label className="landing-action__label">Username</label>
+        </div>
+        <div className="landing-action__password">
+          <input type="password" name="password" id="password"/>
+          <span className="landing-action__highlight"></span>
+          <span className="landing-action__bar"></span>
+          <label className="landing-action__label">Password</label>
+        </div>
+        <div className="landing-action__forgetPwd"></div>
+        <div className="landing-action__signIn"></div>
+        <div className="landing-action__loginLink"></div>
+      </div>
     </div>
   );
 };
