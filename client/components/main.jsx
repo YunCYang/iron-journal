a41b623bd9fb966832f0e2b78dfcaeba7a21166b
YunCYang <ericeorl2013@gmail.com>
2020-06-13 22:44:56 -0700

    basic design of the main page

--- client/components/main.jsx
+++ client/components/main.jsx
@@ -2,7 +2,20 @@ import React from 'react';
 
 const Main = () => {
   return (
-    <></>
+    <div className="main-page">
+      <div className="main-title">
+        <span>Iron Journal</span>
+      </div>
+      <div className="main-menu">
+        <div className="main-menu__game">
+          <span>Game</span>
+        </div>
+        <div className="main-menu__option">
+          <span>Option</span>
+        </div>
+      </div>
+      <div className="main-container"></div>
+    </div>
   );
 };
 
